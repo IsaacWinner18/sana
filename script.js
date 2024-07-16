360425a0c653de672357269633400aaa6b363548
Isaac Winner <149059161+IsaacWinner18@users.noreply.github.com>
2024-07-16 21:14:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const div = document.querySelector('.earningcounter');
-//   const incrementValue = 1000;
-//   const incrementInterval = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+ document.addEventListener("DOMContentLoaded", () => {
+   const div = document.querySelector('.earningcounter');
+   const incrementValue = 1000;
+   const incrementInterval = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
 
-//   function getCurrentValue() {
-//     return parseInt(localStorage.getItem('incrementValue')) || 0;
-//   }
+  function getCurrentValue() {
+    return parseInt(localStorage.getItem('incrementValue')) || 0;
+  }
 
-//   function setCurrentValue(value) {
-//     localStorage.setItem('incrementValue', value);
-//     div.textContent = value;
-//   }
+   function setCurrentValue(value) {
+     localStorage.setItem('incrementValue', value);
+     div.textContent = value;
+   }
 
-//   function checkAndUpdateValue() {
-//     const lastIncrementTime = parseInt(localStorage.getItem('lastIncrementTime')) || 0;
-//     const currentTime = Date.now();
+   function checkAndUpdateValue() {
+     const lastIncrementTime = parseInt(localStorage.getItem('lastIncrementTime')) || 0;
+     const currentTime = Date.now();
 
-//     if (currentTime - lastIncrementTime >= incrementInterval) {
-//       const newValue = getCurrentValue() + incrementValue;
-//       setCurrentValue(newValue);
-//       localStorage.setItem('lastIncrementTime', currentTime);
-//     } else {
-//       setCurrentValue(getCurrentValue());
-//     }
-//   }
+    if (currentTime - lastIncrementTime >= incrementInterval) {
+       const newValue = getCurrentValue() + incrementValue;
+      setCurrentValue(newValue);
+      localStorage.setItem('lastIncrementTime', currentTime);
+     } else {
+      setCurrentValue(getCurrentValue());
+     }
+  }
 
-//   checkAndUpdateValue();
+   checkAndUpdateValue();
 
-//   setInterval(() => {
-//     checkAndUpdateValue();
+   setInterval(() => {
+     checkAndUpdateValue();
 
-//   }, 1000); // Check every second
+   }, 1000); // Check every second
 
 
 
-//  });
+  });
 
 
