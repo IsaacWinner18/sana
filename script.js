24ba02c329888f2c6438efb582739d4bfb4b2273
IsaacWinner18 <winsamlab222@gmail.com>
2024-07-18 10:03:44 -0700

    g

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const div = document.querySelector('.earningcounter');
-  const incrementValue = 30;
-  const incrementInterval = 1 * 60 * 60 * 1000; // 24 hours in milliseconds
-
-  function getCurrentValue() {
-    return parseInt(localStorage.getItem('incrementValue')) || 0;
-  }
-
-  function setCurrentValue(value) {
-    localStorage.setItem('incrementValue', value);
-    div.textContent = value;
-  }
-
-  function checkAndUpdateValue() {
-    const lastIncrementTime = parseInt(localStorage.getItem('lastIncrementTime')) || 0;
-    const currentTime = Date.now();
-
-    if (currentTime - lastIncrementTime >= incrementInterval) {
-      const newValue = getCurrentValue() + incrementValue;
-      setCurrentValue(newValue);
-      localStorage.setItem('lastIncrementTime', currentTime);
-    } else {
-      setCurrentValue(getCurrentValue());
+document.addEventListener('DOMContentLoaded', (event) => {
+    const div = document.querySelector('.earningcounter');
+    const incrementValue = 30;
+    const incrementInterval = 1 * 60 * 60 * 1000; // 1 hour in milliseconds
+  
+    function getCurrentValue() {
+      return parseInt(localStorage.getItem('incrementValue')) || 0;
     }
-  }
-
-  checkAndUpdateValue();
-
-  setInterval(() => {
+  
+    function setCurrentValue(value) {
+      localStorage.setItem('incrementValue', value);
+      div.textContent = value;
+    }
+  
+    function checkAndUpdateValue() {
+      const lastIncrementTime = parseInt(localStorage.getItem('lastIncrementTime')) || 0;
+      const currentTime = Date.now();
+  
+      if (currentTime - lastIncrementTime >= incrementInterval) {
+        const newValue = getCurrentValue() + incrementValue;
+        setCurrentValue(newValue);
+        localStorage.setItem('lastIncrementTime', currentTime);
+      } else {
+        setCurrentValue(getCurrentValue());
+      }
+    }
+  
     checkAndUpdateValue();
-    
-  }, 1000); // Check every second
-});
+  
+    setInterval(() => {
+      checkAndUpdateValue();
+    }, 1000); // Check every second
+  });
+  
