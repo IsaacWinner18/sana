ea9971a0765ec4a7d680a0cf943445c014ae9c47
IsaacWinner18 <winsamlab222@gmail.com>
2024-07-19 03:36:56 -0700

    g

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   
     // Set interval to update balance every hour (3600000 milliseconds)
-    setInterval(updateBalance, 3600000);
+    setInterval(updateBalance, 360);
   
     // Initialize balance on page load
     initializeBalance();
