8c8f2ea56d37a4baac455be9531ae2aa2d2abeb4
IsaacWinner18 <winsamlab222@gmail.com>
2024-07-19 13:40:23 -0700

    k

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
   
     // Function to update balance and store in local storage
     function updateBalance() {
-      let currentBalance = parseFloat(localStorage.getItem("currentBalance")) || 26600;
+      let currentBalance = parseFloat(localStorage.getItem("currentBalance")) || 26750;
       currentBalance += ratePerHour;
       balanceDisplay.textContent = currentBalance;
       localStorage.setItem("currentBalance", currentBalance);
@@ -13,9 +13,9 @@ document.addEventListener("DOMContentLoaded", function() {
   
     // Initialize balance display from local storage
     function initializeBalance() {
-      let savedBalance = parseFloat(localStorage.getItem("currentBalance")) || 26600;
+      let savedBalance = parseFloat(localStorage.getItem("currentBalance")) || 26750;
       balanceDisplay.textContent = savedBalance;
-// localStorage.clear()
+localStorage.clear()
     }
   
     // Set interval to update balance every hour (3600000 milliseconds)
