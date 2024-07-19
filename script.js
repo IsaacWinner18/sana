0befaca5f17b41201d9ba9ae201e4feac81cb531
IsaacWinner18 <winsamlab222@gmail.com>
2024-07-19 04:27:05 -0700

    j

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
   
     // Function to update balance and store in local storage
     function updateBalance() {
-      let currentBalance = parseFloat(localStorage.getItem("currentBalance")) || 26352;
+      let currentBalance = parseFloat(localStorage.getItem("currentBalance")) || 26353;
       currentBalance += ratePerHour;
       balanceDisplay.textContent = currentBalance;
     //   localStorage.setItem("currentBalance", currentBalance);
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
   
     // Initialize balance display from local storage
     function initializeBalance() {
-      let savedBalance = parseFloat(localStorage.getItem("currentBalance")) || 26352;
+      let savedBalance = parseFloat(localStorage.getItem("currentBalance")) || 26353;
       balanceDisplay.textContent = savedBalance;
       localStorage.clear();
 
